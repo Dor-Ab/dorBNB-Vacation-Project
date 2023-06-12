87a24b83f05e3eb5c9e002b523014c1de3d98bf5
Dor Abutbul <Doronabutbul@gmail.com>
2023-06-12 10:13:02 +0300

    changed profile picture for user on followercard

--- Frontend/src/Components/AdminArea/FollowerCard/FollowerCard.tsx
+++ Frontend/src/Components/AdminArea/FollowerCard/FollowerCard.tsx
@@ -1,7 +1,7 @@
 import { Col, Row } from "react-bootstrap";
 import FollowerModel from "../../../Models/followerModel";
 import "./FollowerCard.css";
-import appConfig from "../../../Utils/appConfig";
+import AvatarProfile from "../../../Assets/Images/avatar.png"
 
 interface FollowerCardProps {
     follower: FollowerModel
@@ -12,7 +12,7 @@ function FollowerCard(props: FollowerCardProps): JSX.Element {
         <div className="FollowerCard">
             <Row className="row">
                 <Col className="followerPhotoContainer">
-                    <img src={`${appConfig.vacationImagesUrl}${props.follower.vacationID}`} />
+                    <img src={AvatarProfile} />
                 </Col>
                 <Col className="followerDetails">
                     <h2>{props.follower.firstName} {props.follower.lastName}</h2>
