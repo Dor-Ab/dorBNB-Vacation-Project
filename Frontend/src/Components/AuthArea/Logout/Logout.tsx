bcf3d6cbcf3477b27df5d5a379a2b4c9e7bc31e4
Dor Abutbul <Doronabutbul@gmail.com>
2023-06-12 16:11:16 +0300

    changed pageNotFound

--- Frontend/src/Components/AuthArea/Logout/Logout.tsx
+++ Frontend/src/Components/AuthArea/Logout/Logout.tsx
@@ -3,15 +3,22 @@ import { useNavigate } from "react-router-dom";
 import authService from "../../../Services/authService";
 import notify from "../../../Services/notifyService";
 import "./Logout.css";
+import { authStore } from "../../../Redux/authState";
 
 function Logout(): JSX.Element {
 
     const navTo = useNavigate()
 
     useEffect(() => {
-        authService.logout()
-        notify.error("Hope To See You Soon")
-        navTo("/login")
+        if (authStore.getState().user) {
+            authService.logout()
+            notify.error("Hope To See You Soon")
+            navTo("/login")
+        }
+        else {
+            notify.error("You must login to logout")
+            navTo("/login")
+        }
     }, [])
 
     return null
--- Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
+++ Frontend/src/Components/LayoutArea/PageNotFound/PageNotFound.tsx
@@ -1,9 +1,16 @@
+import { NavLink } from "react-router-dom";
 import "./PageNotFound.css";
 
 function PageNotFound(): JSX.Element {
+
     return (
         <div className="PageNotFound">
-            <h2>404</h2>
+            <h2>Whoops</h2>
+            <div className="msg404">4<div>0</div>4</div>
+            <p>Seems like you got to place that does not exist</p>
+            <p className="ufo404">( maybe the ufo took it 👽<br /> The truth is out there)</p><br />
+            <div className="ufoShip"><span>🛸</span></div>
+            <NavLink to={"/"}>👽 👽 ET Go Home 👽 👽</NavLink>
         </div>
     );
 }
